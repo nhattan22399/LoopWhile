f7c6f6d524ed0e818f38c28d8810a06aa666b20d
nhattan22399 <linhatan99@gmail.com>
2021-05-06 16:38:36 +0700

    first commit

---
+++ src/basic/dev/test.java
@@ -0,0 +1,27 @@
+package basic.dev;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			int i=100;
+			int a,b,c;
+			
+		
+			while(i<1000){
+				int x = i;
+				a= x/100;
+				b= (x%100)/10;
+				c=(x%100)%10;
+				int d = a+ b +c;
+				if (d % 3 == 0) 
+					System.out.print(+i+"\t");
+				i++;
+				
+			
+	
+
+}
+			
+	}}
